84cb499004ea4f58171489ae9780c2a3e459606d
Sevak <128961328+InterPredatorr@users.noreply.github.com>
2026-01-29 21:11:47 +0400

    Fix release URL to use v1.1.0 tag

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Sparsa",
-            url: "https://github.com/sparsa-inc/sparsa-mobile-sdk-ios/releases/download/1.1.0/Sparsa.xcframework.zip",
+            url: "https://github.com/sparsa-inc/sparsa-mobile-sdk-ios/releases/download/v1.1.0/Sparsa.xcframework.zip",
             checksum: "fcbfc1322e991497ffcca6dfdafd287d68ba6e839c63b49b4380e151aa121921"
         )
     ]
